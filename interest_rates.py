6fd855cab7ba303e6633f38214814333f2192ece
gajardos <62527730+gajardos@users.noreply.github.com>
2023-01-30 13:07:34 -0700

    Add files via upload

---
+++ interest_rates.py
@@ -0,0 +1,12 @@
+# Author: Sebastian Gajardo
+# Date: 1/26/2023
+# Course: CS 361 Software Engineering 1
+# Description: Gets interest rate based on credit score using API.
+
+def get_interest_rate(credit_score):
+    """_summary_
+
+    Args:
+        credit_score (_type_): _description_
+    """
+    return
